4a1bfa8c5debe707f626ec9072c7af850c00c71e
Serhii-Kravchenko-2022 <110328675+Serhii-Kravchenko-2022@users.noreply.github.com>
2022-12-01 12:40:30 +0200

    Add abstract class metod

--- Project_Personal_assistant/CLBOT_PY4POWER/console_output.py
+++ Project_Personal_assistant/CLBOT_PY4POWER/console_output.py
@@ -1,6 +1,18 @@
 from tabulate import tabulate
+from abc import abstractmethod, ABC
 
 
-def show_in_console(data: list, headers='firstrow', format='fancy_grid'):
-    print(tabulate(data, headers=headers,
-                   tablefmt=format, showindex='always'))
+class ConsoleOutput(ABC):
+
+    @abstractmethod
+    def show_in_console(self, data: list, headers='firstrow', format='fancy_grid'):
+        pass
+
+
+class TableOutput(ConsoleOutput):
+
+    def show_in_console(self, data: list, headers='firstrow', format='fancy_grid'):
+        print(tabulate(data, headers=headers, tablefmt=format, showindex='always'))
+
+
+show_in_console = TableOutput.show_in_console
